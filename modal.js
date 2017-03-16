1bd825bce8b3e2e9d45dafdc248023fa8cc6b673
Marliana Lara <marliana.lara@gmail.com>
2017-03-16 11:06:36 -0400

    Add close button functionality

--- modal.js
+++ modal.js
@@ -30,22 +30,33 @@ function addPopup() {
   var body = document.body;
   var overlay = document.querySelector('.overlay');
 
-  var loginForm = document.createElement('form');
+  var formWrapper = document.createElement('form');
+  formWrapper.className = "form-wrapper";
+  body.appendChild(formWrapper);
+
+  var loginForm = document.createElement('div');
+  formWrapper.appendChild(loginForm);
   loginForm.innerHTML = '<button class="close-btn">&times;</button><h4>Login to access downloads</h4><input type="text" name="academic_user" placeholder="username"/><input type="password" name="password" placeholder="password"/><input type="submit" value="submit"></input><p class="message">Not registered? <a class="registrationLink" href="#">Create an account</a></p>'
   loginForm.className = "login-form";
-  body.appendChild(loginForm);
+
+  var registrationLink = document.querySelector('.registrationLink');
+  registrationLink.addEventListener('click', function() {
+    replaceFormContents(formWrapper, loginForm);
+  });
 
   var closeBtn = document.querySelector('.close-btn');
-  var registrationLink = document.querySelector('.registrationLink');
-
   closeBtn.addEventListener('click', function() {
-    body.removeChild(loginForm);
-    body.removeChild(overlay);
+    closeForm();
   });
+}
 
-  registrationLink.addEventListener('click', function() {
-    replaceFormContents(loginForm);
-  });
+
+function closeForm() {
+  var formWrapper = document.querySelector('.form-wrapper');
+  var overlay = document.querySelector('.overlay');
+  console.log({formWrapper, overlay});
+  document.body.removeChild(formWrapper);
+  document.body.removeChild(overlay);
 }
 
 function addStyles() {
@@ -54,28 +65,35 @@ function addStyles() {
   var overlay = document.querySelector('.overlay');
 
   addCSSRule(sheet, ".overlay", "position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,.5); z-index: 999; display: fixed; transition: all .5s ease;", 0);
-  addCSSRule(sheet, ".login-form", "font-family: Helvetica, sans-serif; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1000; background: #FFFFFF; min-width: 250px; max-width: 360px; padding: 40px 60px; text-align: center; transition: all 0.3s ease;", 0);
-  addCSSRule(sheet, ".login-form input", "outline: 0; background: #f2f2f2; width: 100%; border: 0; margin: 0 0 15px; padding: 15px; box-sizing: border-box; font-size: 14px;", 0);
-  addCSSRule(sheet, ".login-form input[type='submit']", "text-transform: uppercase; outline: 0; background-color: #465ca8; width: 100%; border: 0; padding: 15px; color: #FFFFFF; font-size: 14px; cursor: pointer;", 0);
-  addCSSRule(sheet, ".login-form input[type='submit']:hover", "background-color: #0099FF;", 0);
-  addCSSRule(sheet, ".login-form h4", "color: #039; font-size: 20px; margin: 0 0 30px 0;", 0);
-  addCSSRule(sheet, ".login-form .message", "color: #b3b3b3; font-size: 12px; margin: 15px 0 0;", 0);
-  addCSSRule(sheet, ".login-form .message a", "color: rgb(0,0,238); text-decoration: none;", 0);
-  addCSSRule(sheet, ".login-form .close-btn", "background: #fff; border: none; font-size: 2em; position: absolute; top: 5px; right: 10px; color: #999; cursor: pointer; outline: none;", 0);
-  addCSSRule(sheet, ".login-form .close-btn:hover", "color: goldenrod", 0);
-  addCSSRule(sheet, ".login-form select", "height: 40px; margin: 0 0 15px; width: 100%;", 0);
+  addCSSRule(sheet, ".form-wrapper", "font-family: Helvetica, sans-serif; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1000; background: #FFFFFF; min-width: 250px; max-width: 360px; padding: 40px 60px; text-align: center; transition: all 0.3s ease;", 0);
+  addCSSRule(sheet, ".form-wrapper input", "outline: 0; background: #f2f2f2; width: 100%; border: 0; margin: 0 0 15px; padding: 15px; box-sizing: border-box; font-size: 14px;", 0);
+  addCSSRule(sheet, ".form-wrapper input[type='submit']", "text-transform: uppercase; outline: 0; background-color: #465ca8; width: 100%; border: 0; padding: 15px; color: #FFFFFF; font-size: 14px; cursor: pointer;", 0);
+  addCSSRule(sheet, ".form-wrapper input[type='submit']:hover", "background-color: #0099FF;", 0);
+  addCSSRule(sheet, ".form-wrapper h4", "color: #039; font-size: 20px; margin: 0 0 30px 0;", 0);
+  addCSSRule(sheet, ".form-wrapper .message", "color: #b3b3b3; font-size: 12px; margin: 15px 0 0;", 0);
+  addCSSRule(sheet, ".form-wrapper .message a", "color: rgb(0,0,238); text-decoration: none;", 0);
+  addCSSRule(sheet, ".form-wrapper .close-btn", "background: #fff; border: none; font-size: 2em; position: absolute; top: 5px; right: 10px; color: #999; cursor: pointer; outline: none;", 0);
+  addCSSRule(sheet, ".form-wrapper .close-btn:hover", "color: goldenrod", 0);
+  addCSSRule(sheet, ".form-wrapper select", "height: 40px; margin: 0 0 15px; width: 100%;", 0);
 }
 
-function replaceFormContents(form) {
-  fadeIn(form);
+function replaceFormContents(formWrapper, form) {
+  var registrationForm = document.createElement('div');
   var registrationContent = '<button class="close-btn">&times;</button><h4>Register</h4><input type="text" name="academic_name" placeholder="Name"/><input type="text" name="academic_email" placeholder="Email"/><input type="text" name="academic_organization" placeholder="Institution"/><select name="academic_research_area" placeholder="Institution"><option value="" disabled selected>Select Research Area</option><option value="biology">Biology</option><option value="chemistry">Chemistry</option><option value="fishery">Fishery</option><option value="animal_genetics">Animal Genetics</option></select><input type="password" name="academic_password" placeholder="Password"/><input type="password" name="academic_password_confirmation" placeholder="Password Confirmation"/><input type="submit" value="Register"/>';
-  form.innerHTML = registrationContent;
+
+  formWrapper.removeChild(form);
+  registrationForm.innerHTML = registrationContent;
+  formWrapper.appendChild(registrationForm);
+  fadeIn(registrationForm);
+
+  var closeBtn = document.querySelector('.close-btn');
+  closeBtn.addEventListener('click', function() {
+    closeForm();
+  });
 }
 
-
-function fadeIn(el, display){
+function fadeIn(el){
   el.style.opacity = 0;
-  // el.style.display = "block";
 
   (function fade() {
     var val = parseFloat(el.style.opacity);
